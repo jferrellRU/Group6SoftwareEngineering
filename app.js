a020822f90c4d4f8d4af1c1f85c02aa71f86f2d2
Samuel Habib <samuel.15.fahim@gmail.com>
2024-11-24 18:02:43 -0500

    finished file structure and app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const http = require('http')
+const http = require('http');
+const routes = require('./routes/routes');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    routes(req, res); // Delegate request to routes
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
