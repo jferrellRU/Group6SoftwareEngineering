33ce792aa1d26986ea48b1481e78d3ab2987c289
jferrellRU <justin.ferrell@rutgers.edu>
2024-12-10 18:56:33 -0500

    added cart, add to cart option productdetails page,

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 const imageRoutes = require('./routes/imageRoutes');
+const cartRoutes = require('./routes/cart'); // Import the cart routes
 
 // API routes
 app.use('/users', userRoutes);
@@ -20,19 +21,16 @@ app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
 app.use('/reviews', reviewRoutes);
 app.use('/images', imageRoutes);
-
+app.use('/cart', cartRoutes); // Add this route for cart functionality
 
 // Middleware to parse JSON and static files
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// Serve static files 
-
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/style', express.static(path.join(__dirname, 'style')));
 app.use('/jpgs', express.static(path.join(__dirname, 'jpgs')));
-app.use('/reactPage', express.static(path.join(__dirname, 'reactPage')));
-
 
 // Serve index.html for root path
 app.get('/', (req, res) => {
@@ -42,12 +40,6 @@ app.get('/', (req, res) => {
 // Serve the product details page
 app.get('/productDetails.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'productDetails.html'));
-});
-
-// Serve add Product page
-app.use('/addproduct', express.static(path.join(__dirname, 'public/react')));
-app.get('/addproduct', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/react/index.html'));
 });
 
 // Handle 404 for unknown routes
