72d7f281277bc71550b08bdaa1b91bb34027d6d6
evan-ragone <evanpragone@gmail.com>
2024-12-13 21:54:45 -0500

    add orderhistory to model

--- models/userModel.js
+++ models/userModel.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
     isVerified: { type: Boolean, default: false },
     isAdmin: { type: Boolean, default: false },
     address: { type: String, required: false },
+    orderHistory: { type: List, default: [] },
     resetPasswordToken:String,
     resetPasswordExpiresAt: Date,
     verificationToken:String,
